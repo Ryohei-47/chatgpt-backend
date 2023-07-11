b353ba86bb3b9e19b2df759f36f384659cd26c1e
Ryohei-47 <ryo-hei4799@outlook.jp>
2023-07-11 21:48:48 +0900

    controllerの作成

---
+++ src/main/java/com/example/demo/ChatController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+
+    @Autowired
+    private ChatService chatService;
+
+    @RequestMapping(value = "/chat",method = RequestMethod.POST)
+    public String chat(ChatParameter parameter){
+        return chatService.chatCompletions(parameter);
+    }    
+}
