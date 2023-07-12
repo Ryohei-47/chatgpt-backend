83bc0dd3320c852fd86eb2d4d26cfb4a745e3a1b
Ryohei-47 <ryo-hei4799@outlook.jp>
2023-07-12 16:14:07 +0900

    update Controller

--- src/main/java/com/example/demo/ChatController.java
+++ src/main/java/com/example/demo/ChatController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/AzureOpenAI")
 public class ChatController {
 
     @Autowired
